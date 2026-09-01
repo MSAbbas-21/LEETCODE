5261ad5b640825013b94cba2dd220b37c33d677b
Mohammed Abbas <mohammadabbas50605@gmail.com>
2026-09-01 12:53:08 +0530

    Solve LongestPrefix

---
+++ LongestPrefix/LongestPrefix.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        prefix = ""
+        for i in range(len(strs[0])):
+            for word in strs:
+                if i >= len(word) or word[i] != strs[0][i]:
+                    return prefix
+            prefix += strs[0][i]
+        return prefix
