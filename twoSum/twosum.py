99c868c8b833abf01024f7fbdfee9f10ecb5af1f
Mohammed Abbas <mohammadabbas50605@gmail.com>
2026-09-08 14:48:48 +0530

    Optimize Two Sum Using hash map

--- twoSum/twosum.py
+++ twoSum/twosum.py
@@ -1,8 +1,12 @@
 class Solution:
     def twoSum(self, nums, target):
+        seen = {}
 
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
+        for i, num in enumerate(nums):
+            needed = target - num
 
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            if needed in seen:
+                return [seen[needed], i]
+
+            seen[num] = i
+        return seen
